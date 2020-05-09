64d9a4b163cea5fafe4d516569e1e41c42e55d21
Caleb Spain <crspain@uh.edu>
2020-05-09 11:20:59 -0500

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Accordion from './components/Accordion';
+
+new Vue({
+    el: '#app',
+    components: {
+        Accordion
+    },
+    data: {
+        items: [
+            {
+                id: 1,
+                title: 'Title 1',
+                description: 'Description for tab 1'
+            },
+            {
+                id: 2,
+                title: 'Title 2',
+                description: 'Description for tab 2'
+            },
+            {
+                id: 3,
+                title: 'Title 3',
+                description: 'Description for tab 3'
+            },
+            {
+                id: 4,
+                title: 'Title 4',
+                description: 'Description for tab 4'
+            }
+        ]
+    }
+});
---
+++ src/components/Accordion.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <div class="header">
+      <h2>{{ item.title }}</h2>
+      <button type="button" @click="toggle = !toggle">Details</button>
+    </div>
+    <p v-if="toggle">{{ item.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accordion",
+  props: ["item"],
+  data: function() {
+    return {
+      toggle: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  width: 360px;
+  margin: 0px auto;
+  padding: 30px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  border: 2px solid #cdcdcd;
+}
+
+.card:first-of-type {
+  margin-top: 30px;
+}
+
+.card > .header > h2 {
+  float: left;
+}
+
+.card > .header > button {
+  float: right;
+  padding: 10px 20px;
+  color: #fff;
+  background: #004488;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.card > .header:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.card > p {
+  padding-top: 30px;
+  border-top: 1px solid #cdcdcd;
+}
+</style>
